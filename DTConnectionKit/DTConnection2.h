9102029dea013ec9e46ccba2b717d5cd1f2cbe78
Daniel Tull <dt@danieltull.co.uk>
2010-06-30 14:11:50 +0100

    Added methods from the old DTConnection.

--- DTConnectionKit/DTConnection2.h
+++ DTConnectionKit/DTConnection2.h
@@ -17,6 +17,8 @@ typedef enum {
 	DTConnectionPriorityVeryLow
 } DTConnectionPriority;
 
+@protocol DTConnectionDelegate2;
+
 @interface DTConnection2 : NSObject {
 	DTConnectionPriority priority;
 	NSMutableArray *dependencies;
@@ -24,10 +26,12 @@ typedef enum {
 	DTConnectionStatus status;
 	DTURLConnection *urlConnection;
 	NSURL *URL;
+	NSObject *returnedObject;
+	NSError *returnedError;
+	NSURLResponse *returnedResponse;
 }
 
 @property (nonatomic, readonly) DTConnectionStatus status;
-@property (nonatomic, assign) DTConnectionType type;
 @property (nonatomic, assign) DTConnectionPriority priority;
 
 @property (nonatomic, readonly) NSArray *dependencies;
@@ -42,6 +46,152 @@ typedef enum {
 - (void)connect;
 
 - (void)start;
+
+
+#pragma mark -
+#pragma mark Setting up the connection details
+
+/** @name Setting up the connection details
+ @{
+ */
+
+/** @brief The type of connection to use.
+ 
+ Specifies the type of connection to use. DTConnectionType is a typedef enum and possible values can be seen in the header file.
+ */
+@property (nonatomic, assign) DTConnectionType type;
+
+/** @brief This method should be used in subclasses to give custom requests.
+ 
+ Calling super from the subclass will give a mutable request of type 'type', this is the prefered way 
+ to get the request object in subclasses.
+ 
+ @return A URL request which will form the connection.
+ */
 - (NSMutableURLRequest *)newRequest;
 
+/**
+ @}
+ */
+
+
+#pragma mark -
+#pragma mark Setting up the delegate
+
+/** @name Setting up the delegate
+ @{
+ */
+
+/** @brief The object that acts as the delegate of the receiving connection controller.
+ 
+ Unlike the usual behaviour of delegates in Cocoa, the delegate is retained by the connection controller. 
+ This is because DTConnectionController uses DTConnectionManager to perform the connection and the connection manager
+ must retain its delegates. Because of this the delegate should never retain the connection controller.
+ */
+@property (nonatomic, retain) id<DTConnectionDelegate2> delegate;
+
+/**
+ @}
+ */
+
+#pragma mark -
+#pragma mark Handling connection events
+
+/** @name Handling connection events
+ @{
+ */
+
+/** @brief This method should be used in subclasses to handle the returned response.
+ 
+ Subclasses should handle the response, taking any course of action given in the API documentation.
+ The default implementation of this method notifies the delegate and observers of the response, so at
+ the end of this method subclasses should call the super implementation.
+ 
+ @param response The response returned from the connection.
+ */
+- (void)receivedResponse:(NSURLResponse *)response;
+
+/** @brief This method should be used in subclasses to handle the returned data.
+ 
+ Subclasses should handle the incoming data, creating a wrapper or data object for the delegate
+ and observers to use. The default implementation of this method notifies the delegate and observers
+ of the new data. Therefore, at the end of this method subclasses should call the super implementation
+ with the handled data object as the parameter.
+ 
+ @param object The data object returned from the connection.
+ */
+- (void)receivedObject:(NSObject *)object;
+
+/** @brief This method should be used in subclasses to handle the returned error.
+ 
+ Subclasses should handle the error, taking any course of action given in the API documentation.
+ The default implementation of this method notifies the delegate and observers of the error, so at
+ the end of this method subclasses should call the super implementation. This could be provided with
+ a new error message created specifically for delegates and observers or just passing the error 
+ returned from the connection.
+ 
+ @param response The response returned from the connection.
+ */
+- (void)receivedError:(NSError *)error;
+
+/**
+ @}
+ */
+
+#pragma mark -
+#pragma mark Returned connection objects
+
+/** @name Returned connection objects 
+ @{
+ */
+
+/** @brief The response returned from the connection.
+ 
+ This holds the response given to the notifyDelegateAndObserversOfResponse: method, for observers to access.
+ */
+@property (nonatomic, retain, readonly) NSURLResponse *returnedResponse;
+
+/** @brief The object, if there is one, returned from the connection.
+ 
+ This holds the object given to the notifyDelegateAndObserversOfReturnedObject: method, for observers to access.
+ */
+@property (nonatomic, retain, readonly) NSObject *returnedObject;
+
+/** @brief The error, if there is one, returned from the connection.
+ 
+ This holds the error given to the notifyDelegateAndObserversOfReturnedError: method, for observers to access.
+ */
+@property (nonatomic, retain, readonly) NSError *returnedError;
+
+/**
+ @}
+ */
+
+@end
+
+#pragma mark -
+/** The delegate of DTConnectionController must adopt the DTConnectionControllerDelegate protocol, although all the methods 
+ are optional. They allow the delegate to handle only certain types of events, although connectionController:didSucceedWithObject: 
+ and connectionController:didFailWithError: should both be handled to take advantage of the data and handle any occuring errors.
+ */
+@protocol DTConnectionDelegate2
+@optional
+/** @brief Tells the delegate the connection has succeeded.
+ 
+ @param connectionController The connection controller informing the delegate of the event.
+ @param object The object returned by the connection.
+ */
+- (void)dtconnection:(DTConnection2 *)connection didSucceedWithObject:(NSObject *)object;
+/** @brief Tells the delegate the connection has failed.
+ 
+ @param connectionController The connection controller informing the delegate of the event.
+ @param error The error received from the server.
+ */
+- (void)dtconnection:(DTConnection2 *)connection didFailWithError:(NSError *)error;
+/** @brief Tells the delegate a response has been received from the server.
+ 
+ @param connectionController The connection controller informing the delegate of the event.
+ @param response The received response.
+ */
+- (void)dtconnection:(DTConnection2 *)connection didReceiveResponse:(NSURLResponse *)response;
 @end
--- DTConnectionKit/DTConnection2.m
+++ DTConnectionKit/DTConnection2.m
@@ -12,13 +12,24 @@
 @interface DTConnection2 ()
 @property (nonatomic, retain, readwrite) NSURL *URL;
 @property (nonatomic, readwrite) DTConnectionStatus status;
-- (void)dtInternalFinish;
+@property (nonatomic, retain, readwrite) NSObject *returnedObject;
+@property (nonatomic, retain, readwrite) NSError *returnedError;
+@property (nonatomic, retain, readwrite) NSURLResponse *returnedResponse;
+- (void)dt_finish;
+
+- (void)dt_notifyDelegateOfObject:(NSObject *)object;
+- (void)dt_notifyObserversOfObject:(NSObject *)object;
+- (void)dt_notifyDelegateOfReturnedError:(NSError *)error;
+- (void)dt_notifyObserversOfReturnedError:(NSError *)error;
+- (void)dt_notifyDelegateOfResponse:(NSURLResponse *)response;
+- (void)dt_notifyObserversOfResponse:(NSURLResponse *)response;
+
 @end
 
 
 @implementation DTConnection2
 
-@synthesize status, type, priority, URL;
+@synthesize delegate, status, type, priority, URL, returnedObject, returnedError, returnedResponse;
 
 + (DTConnection2 *)connection {
 	return [[[self alloc] init] autorelease];
@@ -70,7 +81,7 @@
 	NSURLRequest *request = [self newRequest];
 	
 	if (!request) {
-		[self dtInternalFinish];
+		[self dt_finish];
 		return;
 	}
 	
@@ -80,17 +91,99 @@
 	
 	self.status = DTConnectionStatusStarted;
 	
-	if (!urlConnection) [self dtInternalFinish];
+	if (!urlConnection) [self dt_finish];
 }
+
+#pragma mark -
+#pragma mark Subclass methods
 
 - (NSMutableURLRequest *)newRequest {
 	NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-	[request setHTTPMethod:DTConnectionTypeString[self.type]];	
+	[request setHTTPMethod:DTConnectionTypeString[type]];	
 	return request;
 }
 
-- (void)dtInternalFinish {
+- (void)receivedObject:(NSObject *)object {
+	self.returnedObject = object;
 	self.status = DTConnectionStatusComplete;
+	[self dt_notifyDelegateOfObject:object];
+	[self dt_notifyObserversOfObject:object];
+}
+
+- (void)receivedResponse:(NSURLResponse *)response {
+	self.returnedResponse = response;
+	self.status = DTConnectionStatusResponded;
+	[self dt_notifyDelegateOfResponse:response];
+	[self dt_notifyObserversOfResponse:response];
+}
+
+- (void)receivedError:(NSError *)error {
+	self.returnedError = error;
+	self.status = DTConnectionStatusFailed;
+	[self dt_notifyDelegateOfReturnedError:error];
+	[self dt_notifyObserversOfReturnedError:error];
+}
+
+#pragma mark -
+#pragma mark NSURLConnection delegate methods
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+    [self receivedResponse:response];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+	[(DTURLConnection *)connection appendData:data];
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    [self receivedObject:((DTURLConnection *)connection).data];
+	[self dt_finish];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+    [self receivedError:error];
+	[self dt_finish];
+}
+
+#pragma mark -
+#pragma mark Private methods
+
+- (void)dt_finish {
+	self.status = DTConnectionStatusComplete;
+}
+
+#pragma mark -
+#pragma mark Private notification methods
+
+- (void)dt_notifyDelegateOfObject:(NSObject *)object {	
+	if ([(NSObject *)self.delegate respondsToSelector:@selector(dtconnection:didSucceedWithObject:)])
+		[self.delegate dtconnection:self didSucceedWithObject:object];
+}
+
+- (void)dt_notifyObserversOfObject:(NSObject *)object {
+	[[NSNotificationCenter defaultCenter] postNotificationName:DTConnectionCompletedNotification object:self];
+}
+
+
+
+- (void)dt_notifyDelegateOfReturnedError:(NSError *)error {
+	if ([(NSObject *)self.delegate respondsToSelector:@selector(dtconnection:didFailWithError:)])
+		[self.delegate dtconnection:self didFailWithError:error];
+}
+
+- (void)dt_notifyObserversOfReturnedError:(NSError *)error {
+	[[NSNotificationCenter defaultCenter] postNotificationName:DTConnectionFailedNotification object:self];
+}
+
+
+
+- (void)dt_notifyDelegateOfResponse:(NSURLResponse *)response {
+	if ([(NSObject *)self.delegate respondsToSelector:@selector(dtconnection:didReceiveResponse:)])
+		[self.delegate dtconnection:self didReceiveResponse:response];
+}
+
+- (void)dt_notifyObserversOfResponse:(NSURLResponse *)response {
+	[[NSNotificationCenter defaultCenter] postNotificationName:DTConnectionResponseNotification object:self];
 }
 
 @end
