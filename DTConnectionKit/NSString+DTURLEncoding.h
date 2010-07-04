b24a9c3b8b4943f2346b860025bf8829bb0b12cc
Daniel Tull <dt@danieltull.co.uk>
2010-07-04 18:42:43 +0100

    Added a category to aid with encoding strings for http.

---
+++ DTConnectionKit/NSString+DTURLEncoding.h
@@ -0,0 +1,15 @@
+//
+//  NSString+DTURLEncoding.h
+//  DTConnectionKit
+//
+//  Created by Daniel Tull on 04.07.2010.
+//  Copyright 2010 Daniel Tull. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface NSString (DTURLEncoding)
+- (NSString *)dt_urlEncodedString;
+
+@end
