b0e6bb706891c351ef0b09740c45a6355b413093
Daniel Tull <dt@danieltull.co.uk>
2010-07-04 10:55:56 +0100

    Added OAuth Signature maker.

---
+++ DTConnectionKit/DTOAuthSignature.h
@@ -0,0 +1,23 @@
+//
+//  DTOAuthSignature.h
+//  DTConnectionKit
+//
+//  Created by Daniel Tull on 04.07.2010.
+//  Copyright 2010 Daniel Tull. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef enum {
+	DTOAuthSignatureTypeHMAC_SHA1 = 0
+} DTOAuthSignatureType;
+
+@interface DTOAuthSignature : NSObject {
+}
+
+@property (nonatomic, assign) DTOAuthSignatureType type;
+@property (nonatomic, copy) NSString *secret;
+@property (nonatomic, copy) NSString *text;
+@property (nonatomic, readonly) NSString *signature;
+
+@end
---
+++ DTConnectionKit/DTOAuthSignature.m
@@ -0,0 +1,41 @@
+//
+//  DTOAuthSignature.m
+//  DTConnectionKit
+//
+//  Created by Daniel Tull on 04.07.2010.
+//  Copyright 2010 Daniel Tull. All rights reserved.
+//
+
+#import "DTOAuthSignature.h"
+#import "NSData+Base64.h"
+#import <CommonCrypto/CommonHMAC.h>
+
+NSString * const DTOAuthSignatureTypeString[] = {
+	@"HMAC-SHA1"
+};
+
+@implementation DTOAuthSignature
+@synthesize type, secret, text;
+
+- (id)nameForType:(DTOAuthSignatureType)aType {
+	return DTOAuthSignatureTypeString[aType];
+}
+
+- (NSString *)signature {
+	
+	NSData *secretData = [self.secret dataUsingEncoding:NSUTF8StringEncoding];
+    NSData *textData = [self.text dataUsingEncoding:NSUTF8StringEncoding];
+	
+	uint8_t result[CC_SHA1_DIGEST_LENGTH] = {0};
+	
+	CCHmacContext hmacContext;
+	CCHmacInit(&hmacContext, kCCHmacAlgSHA1, secretData.bytes, secretData.length);
+	CCHmacUpdate(&hmacContext, textData.bytes, textData.length);
+	CCHmacFinal(&hmacContext, result);
+	
+	NSData *theData = [NSData dataWithBytes:result length:CC_SHA1_DIGEST_LENGTH];
+	
+	return [theData base64EncodedString];
+}
+
+@end
