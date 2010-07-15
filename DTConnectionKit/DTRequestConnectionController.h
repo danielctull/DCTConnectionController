2878d78caa49bafc4d554dc26a5d8c6711f76c8c
Daniel Tull <dt@danieltull.co.uk>
2010-07-15 16:44:19 +0100

    Added a connection controller that handles requests.

---
+++ DTConnectionKit/DTRequestConnectionController.h
@@ -0,0 +1,16 @@
+//
+//  DTRequestConnectionController.h
+//  Car Maps
+//
+//  Created by Daniel Tull on 14.07.2010.
+//  Copyright 2010 Daniel Tull. All rights reserved.
+//
+
+#import "DTConnectionController.h"
+
+
+@interface DTRequestConnectionController : DTConnectionController {}
+
+@property (nonatomic, retain) NSURLRequest *request;
+
+@end
