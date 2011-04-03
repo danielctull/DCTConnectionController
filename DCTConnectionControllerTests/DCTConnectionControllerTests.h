8716f9039b910c2715a32a8d82ceb883d5f9a665
Daniel Tull <dt@danieltull.co.uk>
2011-04-03 16:29:53 +0100

    Add project to make static library

---
+++ DCTConnectionControllerTests/DCTConnectionControllerTests.h
@@ -0,0 +1,17 @@
+//
+//  DCTConnectionControllerTests.h
+//  DCTConnectionControllerTests
+//
+//  Created by Daniel Tull on 3.04.2011.
+//  Copyright 2011 Daniel Tull. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface DCTConnectionControllerTests : SenTestCase {
+@private
+    
+}
+
+@end
