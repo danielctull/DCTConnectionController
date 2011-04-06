9ff346de4f15aa322d657b459340ed4d9dbefdb2
Daniel Tull <dt@danieltull.co.uk>
2011-04-06 23:50:04 +0100

    Add application target

---
+++ DCTConnectionControllerApp/DCTConnectionControllerAppAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  DCTConnectionControllerAppAppDelegate.h
+//  DCTConnectionControllerApp
+//
+//  Created by Daniel Tull on 06/04/2011.
+//  Copyright 2011 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DCTConnectionControllerAppAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
