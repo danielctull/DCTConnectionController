aa783a6a20f4c0c3254599fe81271c8c18e8d63d
Daniel Tull <dt@danieltull.co.uk>
2012-06-14 15:30:32 +0100

    Add static library project

---
+++ DCTConnectionControllerTests/DCTConnectionControllerTests.h
@@ -0,0 +1,13 @@
+//
+//  DCTConnectionControllerTests.h
+//  DCTConnectionControllerTests
+//
+//  Created by Daniel Tull on 14.06.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DCTConnectionControllerTests : SenTestCase
+
+@end
---
+++ DCTConnectionControllerTests/DCTConnectionControllerTests.m
@@ -0,0 +1,32 @@
+//
+//  DCTConnectionControllerTests.m
+//  DCTConnectionControllerTests
+//
+//  Created by Daniel Tull on 14.06.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import "DCTConnectionControllerTests.h"
+
+@implementation DCTConnectionControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DCTConnectionControllerTests");
+}
+
+@end
